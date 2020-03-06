464bd3c8bc0342d12434563358a9c1c988cb683a
DamienWar <damien.codeweb@gmail.com>
2020-03-06 19:20:26 +0100

    init repo

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+# Alias
+alias cr "composer require "
+alias phpd "php -S localhost:8000 -ddisplay_errors=1 -dzend_extension=xdebug.so -dxdebug.remote_enable=1"
+alias sy "php bin/console "
+alias remove "pacaur -Rns"
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/damien/.local/share/omf/themes/bobthefish/fish_prompt.fish
